dae76423e5f4d33e41155c9c00e883ded50205ab
Gaetano Foster <_imahuman@penguin>
2021-12-02 08:42:16 -0500

    Removed compile.c, as it was better suited with shell script and batch. Now, there exists win32_compile.bat and unix_compile.sh. Each will run on its respective operating system, and do the exact same thing.

--- compile.c
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-
-void main(void) {
-    system("gcc -c CGL.c");
-    system("ar -rc libCGL.a CGL.o");
-    system("ranlib libCGL.a");
-}
---
+++ unix_compile.sh
@@ -0,0 +1,4 @@
+gcc -c CGL.c
+ar -rc libCGL.a CGL.o
+ranlib libCGL.a
+echo "Compilation finished"
